136476e54a4d080bd77b7a9c1420933d0a074548
Aly Ibrahim <aly.ibrahim@gmail.com>
2020-09-24 09:47:46 -0400

    Initial Commit

---
+++ src/main/java/com/redhat/demo/SlackApp.java
@@ -0,0 +1,23 @@
+package com.redhat.demo;
+
+
+import com.slack.api.bolt.App;
+import com.slack.api.bolt.servlet.SlackAppServlet;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/slack/events")
+public class SlackApp extends SlackAppServlet {
+  private static final long serialVersionUID = 1L;
+  public SlackApp() throws IOException { super(initSlackApp()); }
+  public SlackApp(App app) { super(app); }
+
+  private static App initSlackApp() throws IOException {
+    App app = new App();
+    app.command("/piglatin", (req, ctx) -> {
+      return ctx.ack("What's up?");
+    });
+    return app;
+  }
+}
