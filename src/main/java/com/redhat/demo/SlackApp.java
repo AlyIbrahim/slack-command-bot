8090db4fc5d5f98103cbbcd58547bbabd86d59f7
alyibrahim <aly.ibrahim@gmail.com>
2020-09-24 21:16:55 +0000

    Add logging

--- src/main/java/com/redhat/demo/SlackApp.java
+++ src/main/java/com/redhat/demo/SlackApp.java
@@ -7,15 +7,33 @@ import com.slack.api.bolt.servlet.SlackAppServlet;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
+
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.jboss.logging.Logger;
+
+import javax.inject.Inject;
+
 @WebServlet("/slack/events")
 public class SlackApp extends SlackAppServlet {
+
+  private static final Logger LOG = Logger.getLogger(SlackApp.class);
+
+  @Inject
+  @Channel("slack")
+  Emitter<PigLatin> slackEmitter;
+
   private static final long serialVersionUID = 1L;
+
   public SlackApp() throws IOException { super(initSlackApp()); }
+
   public SlackApp(App app) { super(app); }
 
   private static App initSlackApp() throws IOException {
     App app = new App();
     app.command("/piglatin", (req, ctx) -> {
+      LOG.info(req.getRequestBodyAsString());
+    
       return ctx.ack("What's up?");
     });
     return app;
